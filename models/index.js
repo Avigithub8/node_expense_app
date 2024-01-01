31b707a1f3d043284c16b4d1ee2bb326f5e79829
Avigithub8 <sharma.avi844@gmail.com>
2024-01-02 02:09:29 +0530

    expense-app project

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_CONNECTION_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOSTNAME,
   dialect: process.env.DB_DIALECT,
-  
+  port:process.env.DB_CONNECTION_PORT
 });
 
 module.exports = sequelize;
